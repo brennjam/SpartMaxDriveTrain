9f8a2cafcedc4d1c32788dd82f48530ea69cb1a5
James Brennhofer <2025brennjam@isd47.org>
2022-02-05 11:35:44 -0600

    Commit

--- src/main/java/frc/robot/commands/DriveWithJoySticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoySticks.java
@@ -54,6 +54,7 @@ public class DriveWithJoySticks extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        
     }
 
     // Called every time the scheduler runs while the command is scheduled.
